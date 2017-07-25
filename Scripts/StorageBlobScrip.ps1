c8bcea31c88f85b39f5cefe4c3a7b8cd64abc6d8
Viral Patel <viral.patel@asos.com>
2017-07-25 17:27:07 +0100

    adding scripts folder

---
+++ Scripts/StorageBlobScrip.ps1
@@ -0,0 +1,34 @@
+﻿$Resource_Group = "viral_test"
+$StorageName = "viralscriptstorage"
+$ContainerName = "armtemplates"
+$fileDirectory = "C:\github\Azure\ARM_VM_Creation"
+
+#Grabbing Storage Account
+$storage = Get-AzureRmStorageAccount -StorageAccountName $StorageName -ResourceGroupName $Resource_Group
+
+#Grabbing Storage Account Key
+$keys = Get-AzureRmStorageAccountKey -ResourceGroupName $Resource_Group -Name $storage.StorageAccountName
+
+#Authenticate
+$storagecontext = New-AzureStorageContext -StorageAccountName $storage.StorageAccountName -StorageAccountKey $keys[0].Value
+
+#Creating Blob Container
+
+New-AzureStorageContainer -Context $storagecontext -Name $ContainerName -Permission Off -Verbose
+
+$container = Get-AzureStorageContainer -Name $ContainerName -Context $storage.Context
+
+if($container)
+{
+    $filestoupload = Get-ChildItem $fileDirectory -Recurse -Force
+    foreach($x in $filestoupload)
+    {
+        $targetpath = (Split-Path -Path $x.FullName -NoQualifier).TrimStart("\")
+        #$targetpath = ($x.FullName.Substring($fileDirectory.Length + 1)).Replace("\","/")
+        Set-AzureStorageBlobContent -File $x.FullName -Container $container.Name -Blob $targetpath -Context $storage.Context -Force -Verbose
+        
+
+    }
+
+
+}
